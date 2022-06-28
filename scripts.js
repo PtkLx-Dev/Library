fb9923a414c58322f20ecb11963fff44e23d126f
Patrick Leroux <leroux.pat@pm.me>
2022-06-28 14:49:27 -0400

    My first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+
+	this.info = function () {
+		console.log(title);
+		console.log(author);
+		console.log(pages);
+		console.log(READ);
+	};
+}
